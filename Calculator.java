4ee2589e8d1e58f1012d8dfcce25d3d054f9f2f4
sagar4java <44398497+sagar4java@users.noreply.github.com>
2022-03-21 16:21:27 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+ 	public void add(int num1, int num2) {
+		System.out.println( "Sum = "  + (num1+num2) ){
+	}
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World from Git");
 		System.out.println("Hi World from Git");
+		Calculator calculator = new Calculator();
+		calculator.add(10,20);
 	}
 }
